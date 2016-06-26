1de73cfe74a1b9e9bdb7550fde2fe56a0504be38
jcblw <jacoblowe2.0@gmail.com>
2016-06-26 13:51:42 -0700

    :octopus: cleaning up file and taking notes
    need to move compile functions into another module for cleaner
    implementation

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ const init = (...args) => args.slice(0, args.length -1)
 const isObject = n => typeof n === 'object' && !Array.isArray(n) && n
 const shallowObjectCompare = (x, y) => Object.keys(x).every(k => x[k] === y[k])
 const shallowArrayCompare = (x, y) => x.every((k, i) => k === y[i])
+// compiled fns move to own module
 const safe = JSON.stringify
-// compiled fn
-// will receive args argument
 const compileIsObject = i => `typeof args[${i}] === 'object' && !Array.isArray(args[${i}]) && args[${i}]`
 const compileShallowObjCompare = (arg, i) => Object.keys(arg)
   .map(key => `args[${i}][${safe(key)}] === ${safe(arg[key])}`)
@@ -51,21 +50,11 @@ const matchesAll = (args) => (arg, i) => {
   return args[i] === arg
 }
 const argMatch = (dictionarys) => (...args) => {
-  // might get clever and generate code for this based on the dictionary
-  // https://www.youtube.com/watch?v=wBOX_i7Z514
-  // const matchesArgs = matchesAll(args)
   for (let dict = 0; dict < dictionarys.length; dict ++) {
     if (dictionarys[dict].match(args)) {
       return dictionarys[dict].handler(...args)
     }
   }
-
-  //   console.log(dictionary.pattern.map(compileExpression).join('\n'))
-  //   if (dictionary.pattern.every(matchesArgs) && !ret) {
-  //     ret = true
-  //     dictionary.handler(...args)
-  //   }
-  // })
 }
 
 function pmatch (patterns = [], appendedPatterns = []) {
