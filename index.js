3712d82d654f03f16ef33586a70c490fd50fd469
jcblw <jacoblowe2.0@gmail.com>
2016-06-26 13:14:00 -0700

    :octopus: generate matching functions for speed and fun

--- index.js
+++ index.js
@@ -12,6 +12,30 @@ const init = (...args) => args.slice(0, args.length -1)
 const isObject = n => typeof n === 'object' && !Array.isArray(n) && n
 const shallowObjectCompare = (x, y) => Object.keys(x).every(k => x[k] === y[k])
 const shallowArrayCompare = (x, y) => x.every((k, i) => k === y[i])
+const safe = JSON.stringify
+// compiled fn
+// will receive args argument
+const compiledIsObject = `typeof arg === 'object' && !Array.isArray(arg) && arg`
+const compileBasic = arg => `arg === ${safe(arg)}`
+const compileShallowObjCompare = arg => Object.keys(arg)
+  .map(key => `arg[${safe(key)}] === ${safe(arg[key])}`)
+  .join(' && ')
+const compileShallowArrayCompare = arr => arr
+  .map((key, i) => `arg[${i}] === ${safe(key)}`)
+  .join(' && ')
+const compileExpression = (arg, i) => {
+  if (isObject(arg)) {
+    return `if (i === ${i} && ${compiledIsObject}) return ${compileShallowObjCompare(arg)}`
+  }
+  if (Array.isArray(arg)) {
+    return `if (i === ${i} && Array.isArray(arg)) return ${compileShallowArrayCompare(arg)}`
+  }
+  if (arg === '*') {
+    return `if (i === ${i}) return true`
+  }
+  return `if (i === ${i}) return arg === ${safe(arg)}`
+}
+
 const matchesAll = (args) => (arg, i) => {
   if (isObject(arg) && isObject(args[i])) {
     return shallowObjectCompare(arg, args[i])
@@ -31,13 +55,20 @@ const argMatch = (dictionarys) => (...args) => {
   // might get clever and generate code for this based on the dictionary
   // https://www.youtube.com/watch?v=wBOX_i7Z514
   let ret;
-  const matchesArgs = matchesAll(args)
+  // const matchesArgs = matchesAll(args)
   dictionarys.forEach((dictionary) => {
-    if (dictionary.pattern.every(matchesArgs) && !ret) {
+    if (args.every(dictionary.match)) {
       ret = true
       dictionary.handler(...args)
     }
   })
+
+  //   console.log(dictionary.pattern.map(compileExpression).join('\n'))
+  //   if (dictionary.pattern.every(matchesArgs) && !ret) {
+  //     ret = true
+  //     dictionary.handler(...args)
+  //   }
+  // })
 }
 
 function pmatch (patterns = [], appendedPatterns = []) {
@@ -57,6 +88,7 @@ pmatch.prototype.of = function (patterns = []) {
 
 pmatch.prototype.when = function (...args) {
   return this.of([{
+    match: new Function('arg', 'i', init(...args).map(compileExpression).join(';')),
     pattern: init(...args),
     handler: tail(...args)
   }])
